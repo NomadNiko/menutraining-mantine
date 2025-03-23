d67a030db61302886ea2c6ad77cdd5f077dd4116
NomadNiko <nomad.niko.inc@gmail.com>
2025-03-22 19:03:48 -1000

    fix: adjusting filters

--- src/components/ingredients/AllergyCheckboxGroup.tsx
+++ src/components/ingredients/AllergyCheckboxGroup.tsx
@@ -60,7 +60,7 @@ export function AllergyCheckboxGroup({
       {isLoading ? (
         <Text size="sm">{t("loading")}</Text>
       ) : (
-        <SimpleGrid cols={{ base: 1, xs: 2, lg: 3 }}>
+        <SimpleGrid cols={{ base: 2, sm: 3, lg: 4 }}>
           {allergies.map((allergy) => (
             <Group key={allergy.allergyId} wrap="nowrap">
               <Checkbox
